ee8ef7f03a12bab10354aeea4c50133d4377dd6a
Guillermo Barredo Garcia <barredo@student.chalmers.se>
2019-10-26 20:39:54 +0200

    Small REST api that allows to see last reported reading

--- iot-tornet-service/app.js
+++ iot-tornet-service/app.js
@@ -1,14 +1,24 @@
 'use strict';
 
 const express = require('express');
+const bodyParser = require('body-parser')
+const jsonParser = bodyParser.json()
+const app = express()
 
-const app = express();
+var latestReading = ""
 
-app.get('/', (req, res) => {
-  res.status(200).send('Hello, world!');
+app.get('/readings', (req, res) => {
+  res.json(latestReading);
+});
+
+app.post('/readings', jsonParser, (req, res) => {
+  latestReading = req.body
+  console.log("Body"+latestReading);
+  res.status(200).send()
 });
 
 if (module === require.main) {
+  app.use(bodyParser.json())
   const server = app.listen(process.env.PORT || 8080, () => {
     const port = server.address().port;
     console.log(`App listening on port ${port}`);
